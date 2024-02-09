0ea3982ceb1c33997f9388a20f585093b06be27b
CamilleusRex <camilleusrex@gmail.com>
2024-02-09 16:20:33 +0100

    changed placemend of CORS operations

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_headers=["*"],  
+)
+
+
 app.include_router(router)
 
 
@@ -19,15 +28,6 @@ init_db()
 
 from api.apis import Contact, create_contact, get_all_contacts, get_contact, update_contact, delete_contact, get_birthdays_within_7_days
 from schemas import ContactCreateUpdate, ContactResponse
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  
-    allow_headers=["*"],  
-)
 
 
 @app.get("/")
